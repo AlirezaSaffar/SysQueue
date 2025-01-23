7108344521a40d2f9ff2263f8d7711765c307a3f
AlirezaSaffar <120209135+AlirezaSaffar@users.noreply.github.com>
2025-01-23 13:44:46 +0330

    Create controler.py

---
+++ controler.py
@@ -0,0 +1,29 @@
+import threading
+import time
+from queue import Queue
+from task import Task
+from core1 import ProcessorCore
+from subsystem1 import Subsystem1
+
+
+
+subsystem1 = Subsystem1(r1_count=5, r2_count=3, time_slice=2)
+
+task1 = Task(task_id=1, execution_time=10, weight=2, required_r1=1, required_r2=1)
+task2 = Task(task_id=2, execution_time=8, weight=3, required_r1=1, required_r2=2)
+task3 = Task(task_id=3, execution_time=12, weight=1, required_r1=1, required_r2=2)
+
+subsystem1.add_task(task1)
+subsystem1.add_task(task2)
+subsystem1.add_task(task3)
+
+subsystem1.start()
+
+   
+waiting_queue_thread = threading.Thread(target=subsystem1.manage_waiting_queue)
+waiting_queue_thread.daemon = True
+waiting_queue_thread.start()
+
+time.sleep(20)
+
+#subsystem1.stop()
