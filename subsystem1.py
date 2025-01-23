2314f5768d37b8b0ab1eb84cf6025cc74a18a6da
AlirezaSaffar <120209135+AlirezaSaffar@users.noreply.github.com>
2025-01-23 13:40:57 +0330

    Update subsystem1.py

--- subsystem1.py
+++ subsystem1.py
@@ -1,39 +1,75 @@
 import threading
-from core import core
+import time
 from queue import Queue
-from queue import PriorityQueue
-def runThread(ReadyQueue,core,number):
-    while True:
-        task=self.ReadyQueue[number].get()
-        self.
+from task import Task
+from core1 import ProcessorCore
+
+class Subsystem1:
+    def __init__(self, r1_count, r2_count, time_slice):
+        self.r1 = r1_count  
+        self.r2 = r2_count 
+        self.time_slice = time_slice 
+        self.ready_queues = [Queue() for _ in range(3)]  
+        self.waiting_queue = Queue()  
+        self.cores = [ProcessorCore(i, self.ready_queues[i], self, time_slice) for i in range(3)]  
+        self.num = 0
+
+    def add_task(self, task):
+        core_id = self.num % 3
+        self.num += 1
         
-    return
-def runCore(ReadyQueue,core):
-    threadCore1= threading.Thread(target=runThread,args=(ReadyQueue,core,0))
-    threadCore2= threading.Thread(target=runThread,args=(ReadyQueue,core,1))    
-    threadCore3= threading.Thread(target=runThread,args=(ReadyQueue,core,2))
-    threadCore1.start()
-    threadCore2.start()
-    threadCore3.start()
-    
-    return
-class subsystem1:
-    def __init__(self,queue):
-        self.core=[]
-        self.core.append(core())
-        self.core.append(core())
-        self.core.append(core())
-        self.WaitingQueue = Queue()
-        self.ReadyQueue = []
-        self.ReadyQueue.append(PriorityQueue())
-        self.ReadyQueue.append(PriorityQueue())
-        self.ReadyQueue.append(PriorityQueue())
-        pass
-    def addTask(self,task):
-        self.WaitingQueue.put(task)
-        return
-    def run(self):
-        thread1=threading.Thread(target=runThread,args=(self.ReadyQueue,self.core))
-        thread1.start()
-        return
+        if 0 <= core_id < len(self.ready_queues):
+            self.ready_queues[core_id].put(task)
+            print(f"Task {task.task_id} added to Core {core_id} Ready Queue")
+        else:
+            print(f"Invalid Core ID: {core_id}")
+
+    def add_to_waiting_queue(self, task):
+       
+        self.waiting_queue.put(task)
+        print(f"Task {task.task_id} added to Waiting Queue")
+
+    def check_resources(self, task):
+       
+        if self.r1 >= task.required_r1 and self.r2 >= task.required_r2:
+            return True
+        return False
+
+    def allocate_resources(self, task):
+       
+        self.r1 -= task.required_r1
+        self.r2 -= task.required_r2
+
+    def release_resources(self, task):
+      
+        self.r1 += task.required_r1
+        self.r2 += task.required_r2
+
+    def start(self):
+        for core in self.cores:
+            core.start()
+
+    def stop(self):
+        for core in self.cores:
+            core.running = False
+        for core in self.cores:
+            core.join()
+
+    def check_waiting_queue(self):
         
+        while not self.waiting_queue.empty():
+            task = self.waiting_queue.queue[0]  
+            if self.check_resources(task):  
+                print(f"Task {task.task_id} moved from Waiting Queue to Ready Queue")
+                self.waiting_queue.get() 
+                core_id = self.num % 3
+                self.ready_queues[core_id].put(task) 
+                task.state = "Ready" 
+            else:
+                break  
+
+    def manage_waiting_queue(self):
+       
+        while True:
+            self.check_waiting_queue()  
+            time.sleep(2)  
