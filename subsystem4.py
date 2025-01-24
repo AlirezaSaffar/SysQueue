9b8153571e03feb3adc7abc317d3e02a40bcecb9
AlirezaSaffar <120209135+AlirezaSaffar@users.noreply.github.com>
2025-01-24 11:06:51 +0330

    Update subsystem4.py

--- subsystem4.py
+++ subsystem4.py
@@ -7,34 +7,58 @@ from core4 import ProcessorCoreFCFS
 
 
 class Subsystem4:
-    def __init__(self):
+    def __init__(self, r1_count, r2_count):
+        self.r1 = r1_count 
+        self.r2 = r2_count  
         self.ready_queue = Queue() 
         self.waiting_queue = Queue()  
-        self.completed_tasks = set()  
+        self.completed_tasks = set() 
         self.lock = threading.Lock() 
-        self.core1 = ProcessorCoreFCFS(1, self) 
+        self.core1 = ProcessorCoreFCFS(1, self)  
         self.core2 = ProcessorCoreFCFS(2, self)  
         self.running = True  
-
     def add_task(self, task):
-        
+       
         with self.lock:
             if all(dep in self.completed_tasks for dep in task.dependencies): 
-                self.ready_queue.put(task)  
-                print(f"Task {task.task_id} added to Ready Queue.")
+                if self.check_resources(task): 
+                    self.allocate_resources(task)
+                    self.ready_queue.put(task)  
+                    print(f"Task {task.task_id} added to Ready Queue.")
+                else:
+                    self.waiting_queue.put(task) 
+                    print(f"Task {task.task_id} added to Waiting Queue due to insufficient resources.")
             else:
-                self.waiting_queue.put(task)  
+                self.waiting_queue.put(task) 
                 print(f"Task {task.task_id} added to Waiting Queue due to unmet dependencies.")
 
+    def check_resources(self, task):
+       
+        return self.r1 >= task.required_r1 and self.r2 >= task.required_r2
+
+    def allocate_resources(self, task):
+        
+        self.r1 -= task.required_r1
+        self.r2 -= task.required_r2
+        print(f"Resources allocated for Task {task.task_id}: R1={task.required_r1}, R2={task.required_r2}")
+
+    def release_resources(self, task):
+        
+        self.r1 += task.required_r1
+        self.r2 += task.required_r2
+        print(f"Resources released for Task {task.task_id}: R1={task.required_r1}, R2={task.required_r2}")
+
     def process_waiting_queue(self):
-         with self.lock:
+        
+        with self.lock:
             for _ in range(self.waiting_queue.qsize()):
                 task = self.waiting_queue.get()
-                if all(dep in self.completed_tasks for dep in task.dependencies):
+                if all(dep in self.completed_tasks for dep in task.dependencies) and self.check_resources(task):
+                    self.allocate_resources(task)
                     self.ready_queue.put(task)
                     print(f"Task {task.task_id} moved from Waiting Queue to Ready Queue.")
                 else:
-                    self.waiting_queue.put(task) 
+                    self.waiting_queue.put(task)  
 
     def start(self):
         
@@ -42,7 +66,7 @@ class Subsystem4:
         self.core2.start()
 
     def stop(self):
-      
+       
         self.running = False
         self.core1.running = False
         self.core2.running = False
